375e8a587acf25cd9205c55e5ab8926f091dc036
Abdelrahman Abdelhafez <abdelrahman.abdelhafez@trivago.com>
2022-01-17 18:05:55 +0100

    Implement sort function

--- src/natural-sort/index.ts
+++ src/natural-sort/index.ts
@@ -1,3 +1,22 @@
-import naturalSort from 'javascript-natural-sort';
+import javascriptNaturalSort from 'javascript-natural-sort';
+import { sort as timsort } from 'timsort';
 
-export { naturalSort };
+/**
+ * Sorts an array in place and returns a reference to the same array.
+ *
+ * @param array - The array to be sorted.
+ * @param comparatorFunction - The function that determines the order of the
+ * elements in the sorted array. It should return a negative value if the first
+ * argument is less than the second argument, zero if they're equal, and a
+ * positive value otherwise.
+ * @returns The sorted array.
+ */
+export const sort = <T>(
+    array: T[],
+    comparatorFunction: (a: T, b: T) => number,
+): T[] => {
+    timsort(array, comparatorFunction);
+    return array;
+};
+
+export const naturalSort = javascriptNaturalSort;
